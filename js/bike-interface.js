11b9b6c6bf5b43be24bb04af5b9bf22542ac6737
Anna Horodetska and Brian Fan <anna+brian@>
2017-05-25 10:11:20 -0700

    add separeted backa and front-end logic

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,6 +1,33 @@
-var apiKey = require('./../.env').apiKey;
+var Bike = require('./../js/bike.js').bikeModule;
+
+var displayBike = function(response){
+  response.bikes.forEach(function(bike){
+    var bikeId = bike.id;
+    var bikeSerial = bike.serial;
+    var bikeManu = bike.manufacturer_name;
+    var bikeLocation = bike.stolen_location;
+    var imageUrl = bike.thumb;
+    var date = new Date(bike.date_stolen*1000);
+    var display =
+      '<div class="col-md-4 mb-3">' +
+        '<div class="card">' +
+          '<img class="card-img-top" src="' + imageUrl + '" alt="Card image cap">' +
+          '<div class="card-block">' +
+            '<h4 class="card-title">Bike ID: ' + bikeId + '</h4>' +
+            "<p>Date Stolen: " + date.toLocaleDateString() + "</p>" +
+            "<p>Manufacturer: " + bikeManu + "</p>" +
+            "<p>Location: " + bikeLocation + "</p>" +
+          '</div>' +
+        '</div>' +
+      '</div>';
+      $('.result').append(display);
+  });
+};
 
 $(document).ready(function() {
+
+  var bikeObject = new Bike();
+
   $('.bike-button').click(function() {
     $('.result').text("");
     var location = $('#location').val();
@@ -11,31 +38,7 @@ $(document).ready(function() {
     $('#manufacturer').val('');
     var distance = $('#distance').val();
 
-    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=5&' + 'manufacturer=' + manufacturer + '&location='+ location + '&distance=' + distance + '&stolenness=proximity&appid=' + apiKey).then(function(response) {
-      console.log(response);
-      response.bikes.forEach(function(bike){
-        var bikeId = bike.id;
-        var bikeSerial = bike.serial;
-        var bikeManu = bike.manufacturer_name;
-        var bikeLocation = bike.stolen_location;
-        var imageUrl = bike.thumb;
-        var date = new Date(bike.date_stolen*1000);
+    bikeObject.getBikeData(location, distance, manufacturer,serial, displayBike);
 
-        console.log(date.toDateString());
-        var result =
-          '<div class="col-md-4 mb-3">' +
-            '<div class="card">' +
-              '<img class="card-img-top" src="' + imageUrl + '" alt="Card image cap">' +
-              '<div class="card-block">' +
-                '<h4 class="card-title">Bike ID: ' + bikeId + '</h4>' +
-                "<p>Date Stolen: " + date.toLocaleDateString() + "</p>" +
-                "<p>Manufacturer: " + bikeManu + "</p>" +
-                "<p>Location: " + bikeLocation + "</p>" +
-              '</div>' +
-            '</div>' +
-          '</div>';
-          $('.result').append(result);
-        });
-      });
     });
 });
---
+++ js/bike.js
@@ -0,0 +1,16 @@
+var apiKey = require('./../.env').apiKey;
+
+function Bike(){
+
+}
+
+Bike.prototype.getBikeData = function(location, distance, manufacturer,serial, displayBike){
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=5&' + 'manufacturer=' + manufacturer + '&location='+ location + '&distance=' + distance + '&stolenness=proximity&appid=' + apiKey).then(function(response) {
+      displayBike(response);
+  
+    }).fail(function(error) {
+    $('.result').text(error.responseJSON.message);
+  });
+};
+
+exports.bikeModule = Bike;
